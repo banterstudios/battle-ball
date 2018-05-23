4994d9d2e2804267573b5a018b78df1fd9057cca
Nick de Rozarieux <banterstudios@gmail.com>
2018-05-23 13:31:58 +0100

    worked on sprite

--- shared/game/components/Sprite/Sprite.js
+++ shared/game/components/Sprite/Sprite.js
@@ -1,6 +1,6 @@
-export default class Sprite {
-  constructor ({ spriteName }) {
-    this.spriteName = name
-    this.type = 'sprite'
+export default {
+  name: 'Sprite',
+  state: {
+    name: ''
   }
 }
